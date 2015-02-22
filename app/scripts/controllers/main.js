ce50ac873296d20d0319239f65147adc173c317f
varguitas <vargasvr94@gmail.com>
2015-02-21 18:35:04 -0600

    Integrando modal con maps

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -34,6 +34,36 @@ function enableMarker(){
     });
 }
 
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+}
+
 
 function initialize() {
   var mapOptions = {
@@ -121,4 +151,6 @@ function toggleMapLayer(element){
                 break;
     }
 }
-	
+	$('#myModal').on('hide.bs.modal', function (e) {
+	  alert("sdf");
+	});
